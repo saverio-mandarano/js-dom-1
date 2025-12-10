5fd1059db4210726b2af5af696ce21bf5cca166b
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-10 16:44:18 +0100

    Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Consegna
+
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+
+// Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+// BONUS
+
+// Facciamo accendi e spegni:
+
+// Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+// Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+// E così via...
+
+// Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 😉
+
