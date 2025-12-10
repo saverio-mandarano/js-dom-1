94c0c62cd0ccb842f8c537b5c963c78067aa0931
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-10 17:03:18 +0100

    prendo elementi con getElementById

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // Consegna
 
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
-
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
 // BONUS
-
 // Facciamo accendi e spegni:
-
 // Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
 // Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
 // E così via...
 
-// Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 😉
+// Per fare questo bonus potremmo aver bisogno di del metodo string.includes() 
+
+
+const lamp = document.getElementById(`lampOff`)
+const btn = document.getElementById(`btnOnOff`);
+
 
